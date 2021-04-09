9b99ea475958c46f0c4ee167e3234d0cb93ef925
Waqar Ali Abbas <70074224+WaqarAliAbbas@users.noreply.github.com>
2021-04-09 21:59:40 +0500

    Update Yp-Data-Scraper.py

--- Yp-Data-Scraper.py
+++ Yp-Data-Scraper.py
@@ -4,6 +4,8 @@ import requests
 import time
 from bs4 import BeautifulSoup
 from colorama import Fore,Back,Style
+import os
+os.system("cls")
 print(f"{Fore.WHITE}\n- DEVELOPER WAQAR ALI ABBAS")
 print(f"{Fore.CYAN}- DEVELOPER CONTACT: +92-3129704496")
 print(f"{Fore.GREEN}- YELLOW PAGES DATA SCRAPER TOOL")
