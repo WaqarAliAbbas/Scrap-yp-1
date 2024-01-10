a6437684a457da74e0408facdc05a412d178f1e7
Waqar Ali Abbas <70074224+WaqarAliAbbas@users.noreply.github.com>
2024-01-10 16:11:39 +0500

    Update Yp-Data-Scraper.py

--- Yp-Data-Scraper.py
+++ Yp-Data-Scraper.py
@@ -7,7 +7,6 @@ from colorama import Fore,Back,Style
 import os
 os.system("clear")
 print(f"{Fore.CYAN}\n\n----> DEVELOPER WAQAR ALI ABBAS")
-print(f"{Fore.WHITE}----> DEVELOPER CONTACT: +92-3129704496")
 print(f"{Fore.GREEN}----> YELLOW PAGES DATA SCRAPER TOOL")
 print(f"{Fore.RED}----> PHANTOM LEGEND")
 print(f'''{Fore.YELLOW}
